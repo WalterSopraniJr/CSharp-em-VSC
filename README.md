02e2e2dc118977eb9219c383fa0069cf79cabe3c
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-04-02 19:34:20 -0300

    Inicio de códigos na linguagem C#.

---
+++ README.md
@@ -0,0 +1,4 @@
+#Desenvolvimento de códigos em C#
+
+No início do curso de Mecatrônica, tive contato com a linguagem C, mas agora o objetivo é mergulhar em C# utilizando .NET, sendo capaz de desenvolver códigos para back-end.
+
---
+++ ola-mundo.cs
@@ -0,0 +1,40 @@
+//Imprime cada uma em uma linha
+Console.WriteLine("Hello ");
+Console.WriteLine("world!");
+
+
+Console.WriteLine("\n");
+
+//	\n - pula linha
+Console.WriteLine("Hello \nworld!");
+
+
+Console.WriteLine("\n");
+
+//	\t - espaçamento(tab)
+Console.WriteLine("Hello \tworld!");
+
+
+Console.WriteLine("\n");
+
+//	\\ - utilizado quando se quer printar a barra, as barras duplas printam apenas uma \ no resultado.
+Console.WriteLine("Hello\\world!");
+
+
+Console.WriteLine("\n");
+
+//	"\"\ - para utilizar aspas dentro de uma frase a ser impressa.
+Console.WriteLine("Hello \"New\" world!");
+
+
+Console.WriteLine("\n");
+
+//	@ - antes das aspas dentro do colsole, deixa a impressão no mesmo formato que a declaração no Console.
+Console.WriteLine(@"Hello 	
+			world!");
+
+
+Console.WriteLine("\n");
+
+//	\u - seguido do código de caracteres de outras linguas, seguindo a tabela unicode.
+Console.WriteLine("\u0048\u0065\u006c\u006c\u006f world!");
